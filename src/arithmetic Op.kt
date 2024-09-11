0edf6cf679590cf16f57607f01eae7c14b5f3e3a
Yuvaraj-Rathod <165530846+Yuvaraj-Rathod@users.noreply.github.com>
2024-09-11 22:53:31 +0530

    initial commit1

--- src/arithmetic Op.kt
+++ src/arithmetic Op.kt
@@ -2,32 +2,31 @@ import java.lang.classfile.constantpool.DoubleEntry
 import kotlin.math.pow
 
 fun main(args: Array<String>) {
-//    val i=13
-//    val j=2
-//    var k=10
-//    println(i.toFloat()/j)
-//    println(j%i)
-//    println(k++ + ++k)
-       val isWhat = true
-       println(!!isWhat)
 
     logicalAndShortCircuit()
+    
     ifElseLadderWhen()
-//    whileDoWhile()
     println(inlineFunctionSum(2,5))
+    
     defaultArgument()
     defaultArgument(10)
+    
     println(namedArguments(b=7,a=8))
+    
     println(funOverloading(22,22))
     println(funOverloading(22.2,22.2))
-//    val fn : (a:Int,b:Int)->Int = ::variable
+
     var fn = :: variable
     println(fn(3,6))
+    
     val fn2 = :: power
     println(fn2(2,10))
+    
     val fn3 : (Int,Int) -> Int = { a, b -> a.toDouble().pow(b).toInt() }
     println(fn3(2,3))
 }
+
+//---------------------------------------------------------------------
 fun logicalAndShortCircuit() {
     val above70 = true
     val androidDev = true
@@ -36,6 +35,7 @@ fun logicalAndShortCircuit() {
     println(PlacementDrive)
 
 }
+//---------------------------------------------------------------------
 fun ifElseLadderWhen(){
     val result = if(2>3)  "Good Morning" else "Good Afternoon"
     println(result)
@@ -47,6 +47,7 @@ fun ifElseLadderWhen(){
         else -> println("no animal found")
     }
 }
+//---------------------------------------------------------------------
 fun whileDoWhile(){
     var i = 1
     var num = readLine()!!.toInt()
@@ -55,19 +56,26 @@ fun whileDoWhile(){
         i++
     }
 }
+//---------------------------------------------------------------------
 fun inlineFunctionSum(num : Int, num2:Int) : Int = num+num2  //we can remove type also as we know it'll return int
+//---------------------------------------------------------------------
 fun defaultArgument(num : Int =2) {
     println(num)
 }
+//---------------------------------------------------------------------
 fun namedArguments(a : Int, b: Int) = a+b
+//---------------------------------------------------------------------
 fun funOverloading(a:Int,b:Int) = a+b
+//---------------------------------------------------------------------
 fun funOverloading(a:Double,b:Double) = a+b
+//---------------------------------------------------------------------
 fun variable(a:Int,b:Int) : Int{
     return a+b
 }
+//---------------------------------------------------------------------
 fun power(a: Int=2, b: Int=2): Int {
     return a.toDouble().pow(b).toInt()
 }
-
+//---------------------------------------------------------------------
 
 
